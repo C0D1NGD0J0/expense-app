f89f28ff655b816bd6b3c3207e11a0865fc9c309
c0d1ngd0j0 <sam.gdouglas@gmail.com>
2024-06-07 06:59:39 -0400

    postgres + docker setup

---
+++ prisma/migrations/20240607104459_user_and_transction_table/migration.sql
@@ -0,0 +1,51 @@
+-- CreateEnum
+CREATE TYPE "PaymentTypeEnum" AS ENUM ('CARD', 'CASH');
+
+-- CreateEnum
+CREATE TYPE "CategoryEnum" AS ENUM ('SAVINGS', 'EXPENSE', 'INVESTMENT');
+
+-- CreateEnum
+CREATE TYPE "StatusEnum" AS ENUM ('PENDING', 'COMPLETED', 'FAILED', 'INITIATE');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "avatar" TEXT,
+    "location" TEXT,
+    "postCode" TEXT NOT NULL,
+    "dob" TIMESTAMP(3),
+    "passwordResetToken" TEXT,
+    "passwordResetTokenExpiresAt" INTEGER,
+    "computedLocation" JSONB,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Transaction" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "amount" INTEGER NOT NULL,
+    "location" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
+    "senderBankId" TEXT,
+    "receiverBankId" TEXT,
+    "accountId" TEXT,
+    "status" "StatusEnum" NOT NULL DEFAULT 'INITIATE',
+    "category" "CategoryEnum" NOT NULL DEFAULT 'EXPENSE',
+    "paymentType" "PaymentTypeEnum" NOT NULL DEFAULT 'CARD',
+
+    CONSTRAINT "Transaction_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Transaction" ADD CONSTRAINT "Transaction_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
